9936d68507e6b221d0130743ec43ee4b5810c245
btowne6907 <briantowne03@gmail.com>
2026-06-21 09:37:33 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import ac_engine
 
 # 1. Page Configuration
 st.set_page_config(page_title="Industrial Climate Simulator", layout="wide")
-
+st.image("logo.jpg", width=250)
 st.title("ERS AC Simulator")
 st.subheader("Created by Brian Towne")
 
